292f3cde0d097bce692a6bbe74701dd32ea7d717
chensheng <chensheng8085497@126.com>
2021-01-24 16:16:06 +0800

    huyong

---
+++ README.md
@@ -0,0 +1,2 @@
+react项目客户端，拿到项目先在根目录npm install。
+等启动服务端成功后，再npm start。
---
+++ src/pages/product/detail.jsx
@@ -0,0 +1,111 @@
+import React, {Component} from 'react'
+import {
+  Card,
+  Icon,
+  List
+} from 'antd'
+
+import LinkButton from '../../components/link-button'
+import {BASE_IMG_URL} from '../../utils/constants'
+import {reqCategory} from '../../api'
+import memoryUtils from "../../utils/memoryUtils";
+
+const Item = List.Item
+
+
+/*
+Product的详情子路由组件
+ */
+export default class ProductDetail extends Component {
+
+  state = {
+    cName1: '',
+    cName2: '',
+  }
+
+  async componentDidMount () {
+    const {pCategoryId, categoryId} = memoryUtils.product
+    if(pCategoryId==='0') {
+      const result = await reqCategory(categoryId)
+      const cName1 = result.data.name
+      this.setState({cName1})
+    } else {
+      const results = await Promise.all([reqCategory(pCategoryId), reqCategory(categoryId)])
+      const cName1 = results[0].data.name
+      const cName2 = results[1].data.name
+      this.setState({
+        cName1,
+        cName2
+      })
+    }
+
+  }
+
+  componentWillUnmount () {
+    memoryUtils.product = {}
+  }
+
+
+  render() {
+
+    const {name, desc, price, detail, imgs} = memoryUtils.product
+    const {cName1, cName2} = this.state
+
+    const title = (
+      <span>
+        <LinkButton>
+          <Icon
+            type='arrow-left'
+            style={{marginRight: 10, fontSize: 20}}
+            onClick={() => this.props.history.goBack()}
+          />
+        </LinkButton>
+
+        <span>商品详情</span>
+      </span>
+    )
+    return (
+      <Card title={title} className='product-detail'>
+        <List>
+          <Item>
+            <span className="left">商品名称:</span>
+            <span>{name}</span>
+          </Item>
+          <Item>
+            <span className="left">商品描述:</span>
+            <span>{desc}</span>
+          </Item>
+          <Item>
+            <span className="left">商品价格:</span>
+            <span>{price}元</span>
+          </Item>
+          <Item>
+            <span className="left">所属分类:</span>
+            <span>{cName1} {cName2 ? ' --> '+cName2 : ''}</span>
+          </Item>
+          <Item>
+            <span className="left">商品图片:</span>
+            <span>
+              {
+                imgs.map(img => (
+                  <img
+                    key={img}
+                    src={BASE_IMG_URL + img}
+                    className="product-img"
+                    alt="img"
+                  />
+                ))
+              }
+            </span>
+          </Item>
+          <Item>
+            <span className="left">商品详情:</span>
+            <span dangerouslySetInnerHTML={{__html: detail}}>
+            </span>
+          </Item>
+
+        </List>
+      </Card>
+    )
+  }
+}
